2ae470446f19e0a767720c834cbb78c6520deb1b
Marlos Ribeiro <marlos.mgrb@gmail.com>
2024-07-13 01:38:46 +0000

    Add router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/home";
+import About from "./pages/about";
 
 function App() {
-  return <h1>Olá Mundo!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<div>404 - Not Found!</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
