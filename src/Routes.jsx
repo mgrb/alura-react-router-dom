95348b485f1930d1b4d5838a2a2220649fb0940f
Marlos Ribeiro <marlos.mgrb@gmail.com>
2024-07-13 20:08:18 +0000

    ajustes de navegação de NotFound para Posts

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -15,8 +15,8 @@ function AppRoutes() {
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="posts/:id" element={<Post />} />
         </Route>
+        <Route path="posts/:id" element={<Post />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Rodape />
--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -3,25 +3,38 @@ import posts from "@/json/posts.json";
 import PostModelo from "@/components/PostModelo";
 import ReactMarkdown from "react-markdown";
 import "./Post.css";
+import NotFound from "../NotFound";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import PaginaPadrao from "@/components/PaginaPadrao";
 
 const Post = () => {
   const { id } = useParams();
   const post = posts.find((post) => post.id === Number(id));
 
   if (!post) {
-    return <h1>Post não encontrado</h1>;
+    return <NotFound />;
   }
 
   return (
-    <PostModelo
-      fotoCapa={`/assets/posts/${post.id}/capa.png`}
-      titulo={post.titulo}
-      key={post.id}
-    >
-      <div className="post-markdown-container">
-        <ReactMarkdown>{post.texto}</ReactMarkdown>
-      </div>
-    </PostModelo>
+    <Routes>
+      <Route path="*" element={<PaginaPadrao />}>
+        <Route
+          index
+          element={
+            <PostModelo
+              fotoCapa={`/assets/posts/${post.id}/capa.png`}
+              titulo={post.titulo}
+              key={post.id}
+            >
+              <div className="post-markdown-container">
+                <ReactMarkdown>{post.texto}</ReactMarkdown>
+              </div>
+            </PostModelo>
+          }
+        />
+      </Route>
+    </Routes>
   );
 };
 
